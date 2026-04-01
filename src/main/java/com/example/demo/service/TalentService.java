f3af035efccf208d815da1bf47bfec6116fe20ea
uba531 <uba7209@gmail.com>
2026-04-01 13:17:21 +0900

    TalentServiceを作成

---
+++ src/main/java/com/example/demo/service/TalentService.java
@@ -0,0 +1,31 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Talent;
+import com.example.demo.form.TalentForm;
+import com.example.demo.repository.TalentRepository;
+
+@Service // これを付けることでSpringの管理対象（職人）になります
+public class TalentService {
+
+    @Autowired
+    private TalentRepository talentRepository;
+
+    // 一覧を取得
+    public List<Talent> selectAll() {
+        return talentRepository.findAll();
+    }
+
+    // FormをEntityに変換して保存する職人の仕事
+    public void insert(TalentForm form) {
+        Talent talent = new Talent();
+        talent.setTalentName(form.getTalentName());
+        talent.setReason(form.getReason());
+        
+        talentRepository.save(talent);
+    }
+}
