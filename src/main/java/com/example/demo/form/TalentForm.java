301fa153646a5c7d2e0dfd6a8e97e1e56c59b3e6
uba531 <uba7209@gmail.com>
2026-04-01 16:32:52 +0900

    Talent.formに主キー（ID）追加

--- src/main/java/com/example/demo/form/TalentForm.java
+++ src/main/java/com/example/demo/form/TalentForm.java
@@ -6,11 +6,15 @@ import lombok.Data;
 
 @Data
 public class TalentForm {
+	
+	
+	private Integer id; 
 
 	@NotBlank(message = "タレント名を入力してください")
 	private String talentName;
 
 	@NotBlank(message = "好きな理由を入力してください")
 	private String reason;
-
+	
+	
 }
