2d13bfa2923c6d1859d2e7e50552ebe84a7ce855
uba531 <uba7209@gmail.com>
2026-04-03 14:05:50 +0900

    TalentFormにサイズのバリテーションを追加

--- src/main/java/com/example/demo/form/TalentForm.java
+++ src/main/java/com/example/demo/form/TalentForm.java
@@ -1,6 +1,7 @@
 package com.example.demo.form;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -10,9 +11,11 @@ public class TalentForm {
 	
 	private Integer id; 
 
+	@Size(max = 15, message = "タレント名は50文字以内で入力してください")
 	@NotBlank(message = "タレント名を入力してください")
 	private String talentName;
-
+	
+    @Size(max = 200, message = "好きな理由は200文字以内で入力してください")
 	@NotBlank(message = "好きな理由を入力してください")
 	private String reason;
 	
