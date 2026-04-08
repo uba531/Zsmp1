ad747048c642314bd7ac92c28cdd8905c719d2cb
uba531 <uba7209@gmail.com>
2026-04-08 17:29:17 +0900

    テスト環境の整備

--- src/test/java/com/example/demo/service/TalentServiceTest.java
+++ src/test/java/com/example/demo/service/TalentServiceTest.java
@@ -2,6 +2,8 @@ package com.example.demo.service;
 
 import static org.junit.jupiter.api.Assertions.*; // 検証用メソッド
 
+import jakarta.transaction.Transactional;
+
 import org.junit.jupiter.api.Test; // テスト用アノテーション
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,14 +18,13 @@ class TalentServiceTest {
 	private TalentService talentService;
 
 	@Test
+	@Transactional // トランザクション管理（テスト後にロールバックされる）
 	void testFindById() {
 		// 【追加】まずテストデータを1件登録する（準備）
 		TalentForm form = new TalentForm();
 		form.setTalentName("テストタレント");
 		form.setReason("テスト理由");
-		talentService.insert(form); // これでDBに1件入る（通常はID:1になる）
-
-		
+		talentService.insert(form); // これでDBに1件入る
 		Talent result = talentService.findById(1);
 
 		// 3. 検証
